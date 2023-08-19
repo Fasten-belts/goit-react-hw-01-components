c3985419a68dde612f1c31940f483a2c95642283
Fasten-belts <124335932+Fasten-belts@users.noreply.github.com>
2023-08-19 23:21:13 +0200

    correct data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
+import userCards from 'data/user.json';
+import transactionsList from 'data/transactions.json';
+import statisticLists from 'data/data.json';
+import friendsList from 'data/friends.json';
 import { ProfileCards } from './ProfileCards/profileCard/ProfileCards';
-import userCards from '../user.json';
 import { Statistics } from './Statistics/statistics/Statistics';
-import statisticLists from '../data.json';
 import { FriendList } from './FriendList/friendList/FriendList';
-import friendsList from '../friends.json';
 import { TransactionHistory } from './Transaction/transactionHistory/TransactionHistory';
-import transactionsList from '../transactions.json';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout';
+
 export const App = () => {
   return (
     <Layout>
